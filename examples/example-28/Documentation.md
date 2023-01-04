2bd0b612bea14c927cbc3325b38067e17e37fe6c
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2023-01-04 18:44:30 +0100

    SVG Tutorial

---
+++ examples/example-28/Documentation.md
@@ -0,0 +1,7 @@
+# Addition
+
+Aan package.json d3 toevoegen door
+
+```shell
+npm install --save d3
+```
--- examples/example-28/src/main.js
+++ examples/example-28/src/main.js
@@ -1,9 +1,7 @@
-import { select, version } from 'd3'; // look for browser global d3 instead of file
+import { select } from 'd3'; // look for browser global d3 instead of file
 
 import something, { somethingElse } from "./something.js";
 
 console.log('Greetings ' + something + ' ' + somethingElse); 
             
-console.log('d3: ', version);
-
 console.log('d3 select: ', select);
