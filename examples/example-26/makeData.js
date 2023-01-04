5f0a4d87fd732e8435a5f75800b3f5b1f138b5f7
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2023-01-04 11:14:50 +0100

    SVG Tutorial

--- examples/example-26/makeData.js
+++ examples/example-26/makeData.js
@@ -24,7 +24,7 @@ export function makeData(n, t) {
     x: d * 60 + 50,
     y: 250 + Math.sin(d * 0.5 + t) * 220,
     r: 20 + Math.sin(d * 0.5 + 6*2) * 10,
-    fill: 'rgb(' + red(t) + ',' + green(t) + ',' + blue(t) + ')'
+    fill: `rgb(${red(t)},${green(t)},${blue(t)})`
   }));
   
   return data;
