08de2da5f4280e390071ab0320602e1eb5d7846e
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2023-02-04 19:27:03 +0100

    time schedule adapted

--- examples/example-36/src/data.js
+++ examples/example-36/src/data.js
@@ -1,56 +1,70 @@
 
 const stringTimes = [
   {
-    tijdstipRegistratie: '2020-01-01T08:00:00',
-    eindRegistratie:  '2020-02-01T08:00:00',
-    beginGeldigheid: '2020-01-02',
+    tijdstipRegistratie: '2020-01-02T08:00:00',
+    eindRegistratie:  '2020-02-02T08:00:00',
+    beginGeldigheid: '2020-01-01',
     eindGeldigheid: null,
+    beginInwerking: '2020-01-01',
+    eindInwerking: null,
     value: 'versie 1',
   },
 
   {
-    tijdstipRegistratie: '2020-02-01T08:00:00',
+    tijdstipRegistratie: '2020-02-02T08:00:00',
     eindRegistratie: null,
-    beginGeldigheid: '2020-01-02',
-    eindGeldigheid: '2020-02-02',
+    beginGeldigheid: '2020-01-01',
+    eindGeldigheid: '2020-02-01',
+    beginInwerking: '2021-01-01',
+    eindInwerking: '2021-02-01',
     value: 'versie 1',
   },
   {
-    tijdstipRegistratie: '2020-02-01T08:00:00',
-    eindRegistratie: '2020-03-01T08:00:00',
-    beginGeldigheid: '2020-02-02',
+    tijdstipRegistratie: '2020-02-02T08:00:00',
+    eindRegistratie: '2020-03-02T08:00:00',
+    beginGeldigheid: '2020-02-01',
     eindGeldigheid: null,
+    beginInwerking: '2021-02-01',
+    eindInwerking: null,
     value: 'versie 2',
   },
     
   {
-    tijdstipRegistratie: '2020-03-01T08:00:00',
+    tijdstipRegistratie: '2020-03-02T08:00:00',
     eindRegistratie: null,
-    beginGeldigheid: '2020-02-02',
-    eindGeldigheid: '2020-03-02',
+    beginGeldigheid: '2020-02-01',
+    eindGeldigheid: '2020-03-01',
+    beginInwerking: '2021-02-01',
+    eindInwerking: '2021-03-01',
     value: 'versie 2',
   },
   
   {
-    tijdstipRegistratie: '2020-03-01T08:00:00',
-    eindRegistratie: '2020-04-01T08:00:00',
-    beginGeldigheid: '2020-03-02',
+    tijdstipRegistratie: '2020-03-02T08:00:00',
+    eindRegistratie: '2020-04-02T08:00:00',
+    beginGeldigheid: '2020-03-01',
     eindGeldigheid: null,
+    beginInwerking: '2021-03-01',
+    eindInwerking: null,
     value: 'versie 3',
   },
   
   {
-    tijdstipRegistratie: '2020-04-01T08:00:00',
+    tijdstipRegistratie: '2020-04-02T08:00:00',
     eindRegistratie: null,
-    beginGeldigheid: '2020-03-02',
-    eindGeldigheid: '2020-04-02',
+    beginGeldigheid: '2020-03-01',
+    eindGeldigheid: '2020-04-01',
+    beginInwerking: '2021-03-01',
+    eindInwerking: '2021-04-01',
     value: 'versie 3',
   },
   {
-    tijdstipRegistratie: '2020-04-01T08:00:00',
+    tijdstipRegistratie: '2020-04-02T08:00:00',
     eindRegistratie: null,
-    beginGeldigheid: '2020-04-02',
+    beginGeldigheid: '2020-04-01',
     eindGeldigheid: null,
+    beginInwerking: '2021-04-01',
+    eindInwerking: null,
     value: 'versie 4',
   },  
 ];
@@ -63,9 +77,10 @@ stringTimes.forEach((d) => {
   timeLine.eindRegistratie = (timeLine.eindRegistratie !== null) ? new Date(d.eindRegistratie + 'Z') : null;
   timeLine.beginGeldigheid = new Date(d.beginGeldigheid + 'Z');
   timeLine.eindGeldigheid = (timeLine.eindGeldigheid  !== null) ? new Date(d.eindGeldigheid + 'Z'): null;
+  timeLine.beginInwerking = new Date(d.beginInwerking + 'Z');
+  timeLine.eindInwerking = (timeLine.eindInwerking  !== null) ? new Date(d.eindInwerking + 'Z'): null;
   timeLine.value = d.value;
 
   timeTable.push(timeLine);
 });
-
 
